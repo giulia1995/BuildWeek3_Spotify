a4ee4d7d1a4c5d2e32c6351ada1346253ce84261
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2024-02-01 18:55:57 +0100

    responsive index and album done

--- js/Components.js
+++ js/Components.js
@@ -26,19 +26,19 @@ const editJumbotron = async (id) => {
   const jumbotronContent = `<div class="immagine">
                             <img src="${cover_medium}" alt="" />
                             </div>
-                            <div class="title">
+                            <div class="col title">
                             <p class="fw-bold mt-1">ALBUM</p>
                             <h1 class="fw-bold">${title}</h1>
                             <p class="fw-bold">${artist["name"]}</p>
                             <p class="fw-bold">${label}</p>
-                            <div class="buttons">
-                            <button type="button" class="btn rounded-pill text-dark fw-bold" id="b-play">Play</button>
-                            <button type="button" class="btn rounded-pill text-white fw-bold bg-dark border" id="b-save">Salva</button>
-                            <i class="bi bi-three-dots text-secondary fw-bold" id="dots"></i>
+                            <div class="buttons d-flex">
+                            <button type="button" class="col-lg-2 col-md-2 col-sm-3 btn rounded-pill text-dark fw-bold" id="b-play">Play</button>
+                            <button type="button" class="d-none d-lg-block btn rounded-pill text-white fw-bold bg-dark border" id="b-save">Salva</button>
+                            <i class="d-none d-lg-block bi bi-three-dots text-secondary fw-bold" id="dots"></i>
                             </div>
                             </div>
                             <div class="position-absolute top-0 end-0">
-                            <button type="button" class="btn btn-secondary fw-bold rounded-pill text-secondary" id="hidden">NASCONDI ANNUNCI</button>
+                            <button type="button" class="d-none d-lg-block btn btn-secondary fw-bold rounded-pill text-secondary" id="hidden">NASCONDI ANNUNCI</button>
                             </div>`;
   const jumbotronEdited = document.querySelector(".jumbotron");
   jumbotronEdited.innerHTML = jumbotronContent;
